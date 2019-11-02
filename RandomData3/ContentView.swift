0cc8468e2699af417392c5894a13cc4364643089
Junichirou OKU <jun1ro1@me.com>
2019-11-02 17:54:27 +0900

    Initial Commit

---
+++ RandomData3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomData3
+//
+//  Created by OKU Junichirou on 2019/11/02.
+//  Copyright © 2019 OKU Junichirou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
